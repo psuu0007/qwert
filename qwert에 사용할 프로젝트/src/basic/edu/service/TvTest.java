ea0d7d72465563b9299a2ca7a14d47f3970d829a
psu <psuu0007@naver.com>
2024-06-26 16:36:54 +0900

    깃허브에서 원격 저장소에 이클립스 프로젝트를 반영하기 위해 테스트 중이다
    by me

---
+++ qwert에 사용할 프로젝트/src/basic/edu/service/TvTest.java
@@ -0,0 +1,14 @@
+package basic.edu.service;
+
+public class TvTest {
+
+	public static void main(String[] args) {
+		
+		System.out.println("이클립스");
+		System.out.println("로컬 저장소");
+		System.out.println("원격 저장소");
+		
+		
+	}
+	
+}
