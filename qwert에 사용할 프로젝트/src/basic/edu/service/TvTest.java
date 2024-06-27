ea64d34e8995aab72302602bab213b25b1e2e32b
psu <psuu0007@naver.com>
2024-06-27 11:38:36 +0900

    Merge branch 'main' of https://github.com/psuu0007/qwert.git into main

--- qwert에 사용할 프로젝트/src/basic/edu/service/TvTest.java
+++ qwert에 사용할 프로젝트/src/basic/edu/service/TvTest.java
@@ -13,6 +13,13 @@ public class TvTest {
 		t.info();
 		
 		System.out.println("이게 나야");
+		
+		System.out.println("psu");
+		
+		System.out.println("충돌이 나는 경우가 많은데 그중에 가장 큰 문제는");
+		System.out.println("같은 파일을 건드리는 것이다");
+		
+		
 	}
 	
 }
